5247cebf07002fa76cb74e72156eb9d85f5403d7
Nikolay <NikolayShtipkov@gmail.com>
2019-07-12 14:29:18 +0300

    still fixing

---
+++ JavaScript-Basics&Fundamentals/02 .Data-Types-And-Variables/01. Lab/05.gramphone.js
@@ -0,0 +1,5 @@
+function solve(group, album, song){
+    let time =(group.length * album.length) * song.length / 2;
+    let rotations = Math.ceil(time / 2.5);
+    console.log(`The plate was rotated ${rotations} times.`)
+}
