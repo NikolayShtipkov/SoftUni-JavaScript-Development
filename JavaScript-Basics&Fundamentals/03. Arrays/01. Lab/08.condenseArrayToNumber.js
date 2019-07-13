f834fbf4a732915e577342044b69bab88cb5ce89
Nikolay <NikolayShtipkov@gmail.com>
2019-07-13 12:25:52 +0300

    Commit

--- JavaScript-Basics&Fundamentals/03. Arrays/01. Lab/08.condenseArrayToNumber.js
+++ JavaScript-Basics&Fundamentals/03. Arrays/01. Lab/08.condenseArrayToNumber.js
@@ -2,10 +2,11 @@ function solve(arr) {
     let finalResult = 0;
 
     if (arr.length === 1) {
-        finalResult = `${arr[0]} is already condensed to number`
+        finalResult = arr[0];
     } else {
+        let condensedArr = [];
+
         for(let i = 0; i < arr.length - 1; i++) {
-            let condensedArr = [];
 
             for(let j = 0; j < arr.length; j++) {
                 condensedArr[j] = arr[j] + arr[j + 1];
