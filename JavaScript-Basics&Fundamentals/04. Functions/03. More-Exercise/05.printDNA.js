f112564932bd2570fd0d65cd95f1bfe1c51752b5
Nikolay <NikolayShtipkov@gmail.com>
2019-07-16 12:53:00 +0300

    functions more exercise

---
+++ JavaScript-Basics&Fundamentals/04. Functions/03. More-Exercise/05.printDNA.js
@@ -0,0 +1,46 @@
+function solve (n) {
+    let sequence = 'ATCGTTAGGG'.split('');
+    let counter = 0;
+    let rows = 1;
+
+    for(let i = 0; i < n; i++) {
+        if (rows > 4) {
+            rows = 1;
+        }
+
+        if (rows === 1) {
+            console.log(`**${sequence[counter]}${sequence[counter + 1]}**`);
+            counter += 2;
+            rows++;
+
+            if (counter > 9) {
+                counter = 0;
+            }
+        } else if (rows === 2) {
+            console.log(`*${sequence[counter]}--${sequence[counter + 1]}*`);
+            counter += 2;
+            rows++;
+
+            if (counter > 9) {
+                counter = 0;
+            }
+        } else if (rows === 3) {
+            console.log(`${sequence[counter]}----${sequence[counter + 1]}`);
+            counter += 2;
+            rows++;
+
+            if (counter > 9) {
+                counter = 0;
+            }
+        } else if (rows === 4) {
+            console.log(`*${sequence[counter]}--${sequence[counter + 1]}*`);
+            counter += 2;
+            rows++;
+
+            if (counter > 9) {
+                counter = 0;
+            }
+        }
+    }
+}
+solve(10)
