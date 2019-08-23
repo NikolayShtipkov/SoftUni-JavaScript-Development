1c364fc387ff73d58957ebdd951d87608a953e07
Nikolay <NikolayShtipkov@gmail.com>
2019-08-23 18:03:53 +0300

    object&classes exercises

---
+++ JavaScript-Basics&Fundamentals/06. Objects-And-Classes/03. More-Exercise/01.classLaptop.js
@@ -0,0 +1,28 @@
+function solve() {
+
+    class Laptop {
+        constructor(producer, age, brand, quality) {
+            this.producer = producer;
+            this.age = age;
+            this.brand = brand;
+            this.isOn = false;
+            this.quality = quality;
+        }
+
+        turnOn() {
+            this.isOn = true;
+            this.quality -= 1;
+        }
+
+        turnOff() {
+            this.isOn = false;
+            this.quality -= 1;
+        }
+
+        showInfo() {
+            return console.log(JSON.stringify({producer: this.producer, age: this.age, brand: this.brand}));
+        }
+    }
+}
+
+solve();
